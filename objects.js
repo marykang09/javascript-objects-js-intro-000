dd6e75564a25bbecfd4e549df641a4087646da73
Mary Kang <mary.kang09@gmail.com>
2020-02-12 20:17:29 +0000

    Automatically backed up by Learn

--- objects.js
+++ objects.js
@@ -1,6 +1,6 @@
 var playlist = {artists: "songs"}
 
 function updatePlaylist(playlist, artistName, songTitle){
-  playlist['My Bloody Valentine'] = 'Slowdive'
+  playlist['My Bloody Valentine'] = 'Slowdive';
   return playlist;
 }
